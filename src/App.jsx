6809640cba5cbc9a4d06cd3960aac947c6c7e0bf
Дима Бурлаков <tawerxo@mail.ru>
2023-11-12 03:39:15 +0300

    add images

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,42 @@
 import React from 'react';
 import { Button } from 'react-scroll';
 import './App.scss';
-
-const items = ['Торты', 'Пирожные', 'Пироги', 'Свадебные караваи', 'Булочные изделия'];
-const products = ['', '', '', '', '', '', '', ''];
+const torts = [
+  '20230319_174606.png',
+  '20230323_161208.png',
+  '20230331_122617.png',
+  '20230406_153256.png',
+  '20230407_100708.png',
+  '20230420_170950.png',
+  '20230424_124433.png',
+  '20230505_114453.png',
+  '20230522_163644.png',
+  '20230602_124129.png',
+  '20230602_154701.png',
+  '20230615_124513.png',
+  '20230622_111705.png',
+  '20230630_160035.png',
+  '20230702_120046.png',
+  '20230714_145353.png',
+  '20230716_090011.png',
+  '20230717_150611.png',
+  '20230721_153836.png',
+  '20230813_154732.png',
+  '20230813_164640.png',
+  '20230813_180735.png',
+  '20230818_113602.png',
+  '20230818_125249.png',
+  '20230825_150624.png',
+  '20230825_161746.png',
+  '20230825_181442.png',
+  '20231005_150059.png',
+  '20231005_164516.png',
+  '20231010_115554.png',
+  '20231012_110312 (1).png',
+  '20231012_174658 (1).png',
+];
+// const items = ['Торты', 'Пирожные', 'Пироги', 'Свадебные караваи', 'Булочные изделия'];
+const items = ['Заказные торты'];
 
 const App = () => {
   const [itemSelect, SetItemSelect] = React.useState(0);
@@ -81,26 +114,29 @@ const App = () => {
           </div>
         </div>
         <div className="products">
-          {products.map(() => {
-            return (
-              <div className="product">
-                <image />
-                <p>Название</p>
-              </div>
-            );
+          {torts.map((file, i) => {
+            if (pageSelect <= i && i < pageSelect + 8) {
+              return (
+                <div key={file} className="product">
+                  <img src={require(`./assets/cakes/${file}`)} />
+                  <p>Название</p>
+                </div>
+              );
+            }
           })}
         </div>
         <div className="page_count">
           <ul>
-            {products.map((_, i) =>
-              i <= 5 ? (
-                <li
-                  key={i}
-                  className={i == pageSelect ? 'selected' : ''}
-                  onClick={() => SetPageSelect(i)}>
-                  {i + 1}
-                </li>
-              ) : null,
+            {torts.map(
+              (_, i) =>
+                i % 8 == 0 && (
+                  <li
+                    key={i}
+                    className={i == pageSelect ? 'selected' : ''}
+                    onClick={() => SetPageSelect(i)}>
+                    {i / 8 + 1}
+                  </li>
+                ),
             )}
           </ul>
         </div>
